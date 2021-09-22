d1b7dbe975c56b1b3285c21acf619b784e6c1c55
Morgan Jensen <mbjensen822@outlook.com>
2021-09-22 14:55:38 -0600

    initial commit - program takes input files and produces output .asm and .err files

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace compiler
+{
+    class Program
+    {
+        private static String InputFile { get; set; }
+        private static String ProgramName { get; set; }
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No params given");
+                // throw error
+            }
+
+            InputFile = args[0];
+            ProgramName = "temp";
+
+
+
+            CreateOutput();
+        }
+
+        /// <summary>
+        /// Removes existing .asm and .err files if they exist
+        /// Creates new .asm and .err files
+        /// </summary>
+        private static void CreateOutput()
+        {
+            if (File.Exists($"output/{ProgramName}.asm"))
+            {
+                File.Delete($"output/{ProgramName}.asm");
+            }
+            if (File.Exists($"output/{ProgramName}.err"))
+            {
+                File.Delete($"output/{ProgramName}.err");
+            }
+
+            File.Create($"output/{ProgramName}.asm");
+            File.Create($"output/{ProgramName}.err");
+        }
+    }
+}
